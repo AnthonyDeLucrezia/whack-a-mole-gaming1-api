e0d1a8144e0fc042d7e8879a91401f854251e90f
De Lucrezia Anthony <delucrezia.anthony@gmail.com>
2022-07-20 14:57:31 +0100

    add player and return sorted array

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express();
 app.use(cors({
     origin: '*'
 }));
+app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  
+app.use(express.json());
 
 const players = [];
 
@@ -15,9 +22,35 @@ for (let cpt = 0; cpt < 10; cpt++) {
     });
   }
 
+const sortPlayer = (player1, player2) => {
+    if (player1.score > player2.score) {
+        return -1;
+    }
+  
+    if (player1.score < player2.score) {
+        return 1;
+    }
+  
+    return 0;
+  }
+  
 
-app.get('/leaderboard', (req,res) => {
-    res.status(200).json(players)
+app.post('/api/players', function (req, res) {
+    const newPlayer = req.body.player;
+    const today = new Date();
+    const currentPlayer = {
+      id: `${newPlayer.name}-${newPlayer.score}-${today.getTime()}`,
+      name: newPlayer.name,
+      score: newPlayer.score,
+    };
+    players.push(currentPlayer);
+    const sortedPlayers = players.sort(sortPlayer);
+    return res.status(200).json(sortedPlayers.slice(0,10));
+});
+
+app.get('/api/leaderboard', (req,res) => {
+    const sortedPlayers = players.sort(sortPlayer);
+    res.status(200).json(sortedPlayers.slice(0,10))
 })
 
 app.listen(8080, () => {
